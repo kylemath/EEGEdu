0fb7e3690bcf19b4c388679de343ff248256924a
Kyle Mathewson <kylemath@gmail.com>
2019-11-27 22:44:50 -0700

    changed from frequency bands to plotting the whole spectra as a line graph

--- src/MuseFFT.js
+++ src/MuseFFT.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import { zipSamples, channelNames, MuseClient } from 'muse-js';
-import { powerByBand, epoch, fft } from "@neurosity/pipes";
+import { powerByBand, epoch, fft, sliceFFT } from "@neurosity/pipes";
 
-import { Bar } from "react-chartjs-2";
+import { Bar, Line, Radar } from "react-chartjs-2";
 
 import './MuseFFT.css';
 
@@ -19,21 +19,7 @@ export class MuseFFT extends Component {
     status: 'Disconnected',
     button_disabled: false,
     ch0: {
-	     labels: ["Delta", "Theta", "Alpha", "Beta", "Gamma"],
-	     datasets: [
-    		{
-    			fillColor: "rgba(220,220,220,0.2)",
-    			strokeColor: "rgba(220,220,220,1)",
-    			pointColor: "rgba(220,220,220,1)",
-    			pointStrokeColor: "#fff",
-    			pointHighlightFill: "#fff",
-    			pointHighlightStroke: "rgba(220,220,220,1)",
-    			data: [0,0,0,0,0]
-    		}
-      ]
-    },
-    ch1: {
-       labels: ["Delta", "Theta", "Alpha", "Beta", "Gamma"],
+      // labels: ["LowDelta", "HighDelta", "LowTheta", "HighTheta", "LowAlpha", "HighAlpha", "LowBeta", "HighBeta", "LowGamma", "HighGamma"],
        datasets: [
         {
           fillColor: "rgba(220,220,220,0.2)",
@@ -42,12 +28,24 @@ export class MuseFFT extends Component {
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [0,0,0,0,0]
+        }
+      ]
+    },
+    ch1: {
+       //labels: ["LowDelta", "HighDelta", "LowTheta", "HighTheta", "LowAlpha", "HighAlpha", "LowBeta", "HighBeta", "LowGamma", "HighGamma"],
+       datasets: [
+        {
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
         }
       ]
     },
     ch2: {
-       labels: ["Delta", "Theta", "Alpha", "Beta", "Gamma"],
+       //labels: ["LowDelta", "HighDelta", "LowTheta", "HighTheta", "LowAlpha", "HighAlpha", "LowBeta", "HighBeta", "LowGamma", "HighGamma"],
        datasets: [
         {
           fillColor: "rgba(220,220,220,0.2)",
@@ -56,12 +54,11 @@ export class MuseFFT extends Component {
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [0,0,0,0,0]
         }
       ]
     },
     ch3: {
-       labels: ["Delta", "Theta", "Alpha", "Beta", "Gamma"],
+       //labels: ["LowDelta", "HighDelta", "LowTheta", "HighTheta", "LowAlpha", "HighAlpha", "LowBeta", "HighBeta", "LowGamma", "HighGamma"],
        datasets: [
         {
           fillColor: "rgba(220,220,220,0.2)",
@@ -70,7 +67,6 @@ export class MuseFFT extends Component {
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [0,0,0,0,0]
         }
       ]
     }
@@ -82,10 +78,10 @@ export class MuseFFT extends Component {
           <button disabled={this.state.button_disabled} onClick={this.connect}>Connect Muse Headband</button>
           <p>{this.state.status}</p>
           <div style={chartSectionStyle}>
-            <Bar data={this.state.ch2} options={{title: {display: true, text: 'Channel: ' + channelNames[2]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
-            <Bar data={this.state.ch1} options={{title: {display: true, text: 'Channel: ' + channelNames[1]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
-            <Bar data={this.state.ch0} options={{title: {display: true, text: 'Channel: ' + channelNames[0]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
-            <Bar data={this.state.ch3} options={{title: {display: true, text: 'Channel: ' + channelNames[3]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
+            <Line data={this.state.ch2} options={{title: {display: true, text: 'Channel: ' + channelNames[2]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
+            <Line data={this.state.ch1} options={{title: {display: true, text: 'Channel: ' + channelNames[1]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
+            <Line data={this.state.ch0} options={{title: {display: true, text: 'Channel: ' + channelNames[0]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
+            <Line data={this.state.ch3} options={{title: {display: true, text: 'Channel: ' + channelNames[3]}, responsive: false, tooltips: {enabled: false}, legend: {display: false}}} width={500} height={250}/>
           </div>
         </div>
     );
@@ -111,15 +107,26 @@ export class MuseFFT extends Component {
       zipSamples(client.eegReadings).pipe(
         epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
         fft({ bins: 256 }),
-        powerByBand()
+        sliceFFT([1, 30])
       ).subscribe(
         (data) => {
           this.setState(state => {
 
-            state.ch0.datasets[0].data = [data.delta[0], data.theta[0], data.alpha[0], data.beta[0], data.gamma[0]];
-            state.ch1.datasets[0].data = [data.delta[1], data.theta[1], data.alpha[1], data.beta[1], data.gamma[1]];
-            state.ch2.datasets[0].data = [data.delta[2], data.theta[2], data.alpha[2], data.beta[2], data.gamma[2]];
-            state.ch3.datasets[0].data = [data.delta[3], data.theta[3], data.alpha[3], data.beta[3], data.gamma[3]];
+            state.ch0.datasets[0].data = data.psd[0];
+            state.ch0.xLabels = data.freqs;
+
+            state.ch1.datasets[0].data = data.psd[1];
+            state.ch1.xLabels = data.freqs;
+
+            state.ch2.datasets[0].data = data.psd[2];
+            state.ch2.xLabels = data.freqs;
+
+            state.ch3.datasets[0].data = data.psd[3];
+            state.ch3.xLabels = data.freqs;
+
+            //console.log(data.psd[0].length)
+            console.log(data.freqs)
+
 
             return({
               ch0: state.ch0,
