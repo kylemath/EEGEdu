f3fa00768491c2b29f0db56807294634a0bc01e9
Keyfer Mathewson <keyfermath@gmail.com>
2019-12-07 17:15:05 -0500

    push up

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -19,7 +19,7 @@ export function PageSwitcher() {
   const [status, setStatus] = useState(generalTranslations.connect);
   const [selected, setSelected] = useState(translations.types.raw);
   const handleSelectChange = useCallback(value => {
-    handleUnsubscribe();
+    handleUnsubscribe().then();
     setSelected(value);
   }, []);
   const options = [
@@ -29,7 +29,6 @@ export function PageSwitcher() {
 
   window.rawZippedData$ = null;
   window.rawSubscription$ = null;
-
   window.spectraZippedData$ = null;
   window.spectraSubscription$ = null;
 
@@ -52,73 +51,88 @@ export function PageSwitcher() {
       setStatus(generalTranslations.connected);
 
       if (window.museClient && window.museClient.eegReadings) {
-        window.rawZippedData$ = zipSamples(window.museClient.eegReadings).pipe(
-          bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
-          epoch({ duration: 1024, interval: 50, samplingRate: 256 }),
-          catchError(err => {
-            console.log(err);
-          })
-        );
+        switch (selected) {
+          case translations.types.raw:
+            if (window.rawZippedData$ === null) {
+              window.rawZippedData$ = zipSamples(
+                window.museClient.eegReadings
+              ).pipe(
+                bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+                epoch({ duration: 1024, interval: 50, samplingRate: 256 }),
+                catchError(err => {
+                  console.log(err);
+                })
+              );
+            }
 
-        window.rawSubscription$ = window.rawZippedData$.subscribe(data => {
-          setRawData(rawData => {
-            Object.values(rawData).forEach(
-              (channel, index) => {
-                if (index < 4) {
-                  const sRate = data.info.samplingRate;
+            window.rawSubscription$ = window.rawZippedData$.subscribe(data => {
+              setRawData(rawData => {
+                Object.values(rawData).forEach(
+                  (channel, index) => {
+                    if (index < 4) {
+                      const sRate = data.info.samplingRate;
 
-                  channel.datasets[0].data = data.data[index];
-                  channel.xLabels = customCount(
-                    (1000 / sRate) * data.data[2].length,
-                    1000 / sRate,
-                    -(1000 / sRate)
-                  ).map(function(each_element) {
-                    return Number(each_element.toFixed(0));
+                      channel.datasets[0].data = data.data[index];
+                      channel.xLabels = customCount(
+                        (1000 / sRate) * data.data[2].length,
+                        1000 / sRate,
+                        -(1000 / sRate)
+                      ).map(function(each_element) {
+                        return Number(each_element.toFixed(0));
+                      });
+                    }
+                  },
+                  err => {
+                    console.log(err);
+                  }
+                );
+
+                return {
+                  ch0: rawData.ch0,
+                  ch1: rawData.ch1,
+                  ch2: rawData.ch2,
+                  ch3: rawData.ch3
+                };
+              });
+            });
+            break;
+
+          case translations.types.spectra:
+            if (window.spectraZippedData$ === null) {
+              window.spectraZippedData$ = zipSamples(
+                window.museClient.eegReadings
+              ).pipe(
+                bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+                epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
+                fft({ bins: 256 }),
+                sliceFFT([1, 30]),
+                catchError(err => {
+                  console.log(err);
+                })
+              );
+            }
+
+            window.spectraSubscription$ = window.spectraZippedData$.subscribe(
+              data => {
+                setSpectraData(spectraData => {
+                  Object.values(spectraData).forEach((channel, index) => {
+                    if (index < 4) {
+                      channel.datasets[0].data = data.psd[index];
+                      channel.xLabels = data.freqs;
+                    }
                   });
-                }
-              },
-              err => {
-                console.log(err);
+
+                  return {
+                    ch0: spectraData.ch0,
+                    ch1: spectraData.ch1,
+                    ch2: spectraData.ch2,
+                    ch3: spectraData.ch3
+                  };
+                });
               }
             );
-
-            return {
-              ch0: rawData.ch0,
-              ch1: rawData.ch1,
-              ch2: rawData.ch2,
-              ch3: rawData.ch3
-            };
-          });
-        });
-
-        window.spectraZippedData$ = zipSamples(
-          window.museClient.eegReadings
-        ).pipe(
-          bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
-          epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
-          fft({ bins: 256 }),
-          sliceFFT([1, 30])
-        );
-
-        window.spectraSubscription$ = window.spectraZippedData$.subscribe(
-          data => {
-            setSpectraData(spectraData => {
-              Object.values(spectraData).forEach((channel, index) => {
-                if (index < 4) {
-                  channel.datasets[0].data = data.psd[index];
-                  channel.xLabels = data.freqs;
-                }
-              });
-
-              return {
-                ch0: spectraData.ch0,
-                ch1: spectraData.ch1,
-                ch2: spectraData.ch2,
-                ch3: spectraData.ch3
-              };
-            });
-          }
-        );
+            break;
+        }
       }
     } catch (err) {
       setStatus(generalTranslations.connect);
@@ -127,10 +141,10 @@ export function PageSwitcher() {
   }
 
   async function handleUnsubscribe() {
-    if (window.rawSubscription$ || window.spectraSubscription$) {
-      window.rawSubscription$.unsubscribe();
-      window.spectraSubscription$.unsubscribe();
-    }
+    window.rawSubscription$ = null;
+    window.spectraSubscription$ = null;
+    window.rawZippedData$ = null;
+    window.spectraZippedData$ = null;
   }
 
   return (
