a39a412b911f5774f93b5757ee1e3e69b422d8de
Kyle Mathewson <kylemath@gmail.com>
2019-12-06 19:38:36 -0700

    created new copy of module folder, renaming and changing everything to make frequency bands

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from "react";
 import { Select, Card } from "@shopify/polaris";
 import EEGEduRaw from "./components/EEGEduRaw/EEGEduRaw";
 import EEGEduSpectra from "./components/EEGEduSpectra/EEGEduSpectra";
+import EEGEduBands from "./components/EEGEduBands/EEGEduBands";
 import * as translations from "./translations/en.json";
 
 export function PageSwitcher() {
@@ -9,13 +10,17 @@ export function PageSwitcher() {
   const handleSelectChange = useCallback(value => setSelected(value), []);
   const options = [
     { label: translations.types.raw, value: translations.types.raw },
-    { label: translations.types.spectra, value: translations.types.spectra }
+    { label: translations.types.spectra, value: translations.types.spectra },
+    { label: translations.types.bands, value: translations.types.bands }
+
   ];
 
   function renderCharts() {
     switch (selected) {
       case translations.types.raw:
         return <EEGEduRaw />;
+      case translations.types.bands:
+        return <EEGEduBands />;        
       default:
         return <EEGEduSpectra />;
     }
---
+++ src/components/PageSwitcher/components/EEGEduBands/EEGEduBands.js
@@ -0,0 +1,176 @@
+import React, { Component } from "react";
+import { channelNames, zipSamples, MuseClient } from "muse-js";
+import { epoch, bandpassFilter, fft, powerByBand } from "@neurosity/pipes";
+import { Button, TextContainer, Card, Stack } from "@shopify/polaris";
+import { Bar } from "react-chartjs-2";
+import { chartAttributes } from "../chartOptions";
+
+import * as generalTranslations from "../translations/en";
+import * as specificTranslations from "./translations/en";
+
+const chartOptions = {
+  scales: {
+    xAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: specificTranslations.xlabel
+        },
+        // ticks: {
+        //   max: 100,
+        //   min: 0
+        // }
+      }
+    ],
+    yAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: specificTranslations.ylabel
+        }
+      }
+    ]
+  },
+  elements: {
+    point: {
+      radius: 0
+    }
+  },
+  title: {
+    display: true,
+    text: generalTranslations.channel
+  },
+  responsive: true,
+  tooltips: { enabled: false },
+  legend: { display: false }
+};
+
+//Labels for the x axis
+var bandLabels = ["Delta", "Theta", "Alpha", "Beta", "Gamma"];
+
+
+export class EEGEduBands extends Component {
+  state = {
+    status: generalTranslations.disconnected,
+    button_disabled: false,
+    channels: {
+      ch0: {
+        labels: bandLabels,
+        datasets: [{}]
+      },
+      ch1: {
+        labels: bandLabels,
+        datasets: [{}]
+      },
+      ch2: {
+        labels: bandLabels,
+        datasets: [{}]
+      },
+      ch3: {
+        labels: bandLabels,
+        datasets: [{}]
+      }
+    }
+  };
+
+  renderCharts() {
+    const { channels } = this.state;
+
+    return Object.values(channels).map((channel, index) => {
+      const tempOptions = {
+        ...chartOptions,
+        title: {
+          ...chartOptions.title,
+          text: generalTranslations.channel + channelNames[index]
+        }
+      };
+
+      return (
+        <Card.Section>
+          <Bar key={index} data={channel} options={tempOptions} />
+        </Card.Section>
+      );
+    });
+  }
+
+  connect = async () => {
+    const client = new MuseClient();
+
+    client.connectionStatus.subscribe(status => {
+      this.setState({
+        status: status
+          ? generalTranslations.connected
+          : generalTranslations.disconnected,
+        button_disabled: status
+      });
+
+      console.log(
+        status
+          ? generalTranslations.connected
+          : generalTranslations.disconnected
+      );
+    });
+
+    try {
+      await client.connect();
+      await client.start();
+
+      zipSamples(client.eegReadings)
+        .pipe(
+          bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+          epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
+          fft({ bins: 256 }),
+          powerByBand()
+        )
+        .subscribe(data => {
+          this.setState(state => {
+            Object.values(state.channels).forEach((channel, index) => {
+              channel.ch0.datasets[0].data = [data.delta[0], data.theta[0], data.alpha[0], data.beta[0], data.gamma[0]];
+              channel.ch1.datasets[0].data = [data.delta[1], data.theta[1], data.alpha[1], data.beta[1], data.gamma[1]];
+              channel.ch2.datasets[0].data = [data.delta[2], data.theta[2], data.alpha[2], data.beta[2], data.gamma[2]];
+              channel.ch3.datasets[0].data = [data.delta[3], data.theta[3], data.alpha[3], data.beta[3], data.gamma[3]];
+            });
+
+            return {
+              ch0: state.channels.ch0,
+              ch1: state.channels.ch1,
+              ch2: state.channels.ch2,
+              ch3: state.channels.ch3
+            };
+          });
+        });
+    } catch (err) {
+      console.error(generalTranslations.connectionFailed, err);
+    }
+  };
+
+  render() {
+    return (
+      <Card title={specificTranslations.title}>
+        <Card.Section>
+          <Stack>
+            <Button
+              primary={this.state.status === generalTranslations.disconnected}
+              disabled={this.state.button_disabled}
+              onClick={this.connect}
+            >
+              {this.state.status === generalTranslations.connected
+                ? generalTranslations.buttonConnected
+                : generalTranslations.buttonToConnect}
+            </Button>
+            <TextContainer>
+              <p>{specificTranslations.description}</p>
+            </TextContainer>
+          </Stack>
+        </Card.Section>
+        <Card.Section>
+          <div style={chartAttributes.wrapperStyle.style}>
+            {this.renderCharts()}
+          </div>
+        </Card.Section>
+      </Card>
+    );
+  }
+}
+
+export default EEGEduBands;
