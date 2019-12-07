d034a1d9f9813f1c25408e13703e5887c5ed5c4b
Keyfer Mathewson <keyfermath@gmail.com>
2019-12-07 16:51:08 -0500

    Add hoyl fuck

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -1,19 +1,25 @@
 import React, { useState, useCallback } from "react";
 
 import { Select, Card, Stack, Button } from "@shopify/polaris";
+import { bandpassFilter, epoch, fft, sliceFFT } from "@neurosity/pipes";
+import { catchError } from "rxjs/operators";
 
 import EEGEduRaw from "./components/EEGEduRaw/EEGEduRaw";
 import EEGEduSpectra from "./components/EEGEduSpectra/EEGEduSpectra";
 
 import * as translations from "./translations/en.json";
-import { MuseClient } from "muse-js";
+import { MuseClient, zipSamples } from "muse-js";
 import * as generalTranslations from "./components/translations/en";
+import { emptyChannelData } from "./components/chartOptions";
+import { customCount } from "./components/chartUtils";
 
 export function PageSwitcher() {
+  const [rawData, setRawData] = useState(emptyChannelData);
+  const [spectraData, setSpectraData] = useState(emptyChannelData);
   const [status, setStatus] = useState(generalTranslations.connect);
   const [selected, setSelected] = useState(translations.types.raw);
   const handleSelectChange = useCallback(value => {
-    console.log((window.shouldUnsubscribe = true));
+    handleUnsubscribe();
     setSelected(value);
   }, []);
   const options = [
@@ -21,13 +27,18 @@ export function PageSwitcher() {
     { label: translations.types.spectra, value: translations.types.spectra }
   ];
 
+  window.rawZippedData$ = null;
+  window.rawSubscription$ = null;
+
+  window.spectraZippedData$ = null;
+  window.spectraSubscription$ = null;
+
   function renderCharts() {
-    window.shouldUnsubscribe = false;
     switch (selected) {
       case translations.types.raw:
-        return <EEGEduRaw />;
+        return <EEGEduRaw data={rawData} />;
       default:
-        return <EEGEduSpectra />;
+        return <EEGEduSpectra data={spectraData} />;
     }
   }
 
@@ -39,9 +50,86 @@ export function PageSwitcher() {
       await window.museClient.connect();
       await window.museClient.start();
       setStatus(generalTranslations.connected);
+
+      if (window.museClient && window.museClient.eegReadings) {
+        window.rawZippedData$ = zipSamples(window.museClient.eegReadings).pipe(
+          bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+          epoch({ duration: 1024, interval: 50, samplingRate: 256 }),
+          catchError(err => {
+            console.log(err);
+          })
+        );
+
+        window.rawSubscription$ = window.rawZippedData$.subscribe(data => {
+          setRawData(rawData => {
+            Object.values(rawData).forEach(
+              (channel, index) => {
+                if (index < 4) {
+                  const sRate = data.info.samplingRate;
+
+                  channel.datasets[0].data = data.data[index];
+                  channel.xLabels = customCount(
+                    (1000 / sRate) * data.data[2].length,
+                    1000 / sRate,
+                    -(1000 / sRate)
+                  ).map(function(each_element) {
+                    return Number(each_element.toFixed(0));
+                  });
+                }
+              },
+              err => {
+                console.log(err);
+              }
+            );
+
+            return {
+              ch0: rawData.ch0,
+              ch1: rawData.ch1,
+              ch2: rawData.ch2,
+              ch3: rawData.ch3
+            };
+          });
+        });
+
+        window.spectraZippedData$ = zipSamples(
+          window.museClient.eegReadings
+        ).pipe(
+          bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+          epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
+          fft({ bins: 256 }),
+          sliceFFT([1, 30])
+        );
+
+        window.spectraSubscription$ = window.spectraZippedData$.subscribe(
+          data => {
+            setSpectraData(spectraData => {
+              Object.values(spectraData).forEach((channel, index) => {
+                if (index < 4) {
+                  channel.datasets[0].data = data.psd[index];
+                  channel.xLabels = data.freqs;
+                }
+              });
+
+              return {
+                ch0: spectraData.ch0,
+                ch1: spectraData.ch1,
+                ch2: spectraData.ch2,
+                ch3: spectraData.ch3
+              };
+            });
+          }
+        );
+      }
     } catch (err) {
       setStatus(generalTranslations.connect);
       console.log(err);
+    }
+  }
+
+  async function handleUnsubscribe() {
+    if (window.rawSubscription$ || window.spectraSubscription$) {
+      window.rawSubscription$.unsubscribe();
+      window.spectraSubscription$.unsubscribe();
     }
   }
 
