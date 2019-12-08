7e976313dadc505c582ae149af09fee1fa817bd9
Kory Mathewson <korymath@gmail.com>
2019-12-08 17:13:32 -0500

    working test version

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from "react";
 
 import { Select, Card, Stack, Button } from "@shopify/polaris";
-import { bandpassFilter, epoch, fft, sliceFFT, createEEG } from "@neurosity/pipes";
+import { bandpassFilter, epoch, fft, sliceFFT } from "@neurosity/pipes";
 import { interval, Subject, of } from "rxjs";
 import { catchError, multicast } from "rxjs/operators";
 
@@ -42,60 +42,70 @@ export function PageSwitcher() {
   }
 
   async function connect() { 
-    console.log('Connecting to createEEG observable...');
+    console.log('Connecting to data source observable...');
     setStatus(generalTranslations.connecting);
     
     try {
-      // create real eeg data source
+      // TODO(kylemath): fix the source here
+      // 1) create real eeg data source
       // window.museClient = new MuseClient();
-      // create mock eeg data source
-      // window.source$ = createEEG();
-      // For debugging
+      // window.source$ = window.museClient.eegReadings;
+      
+      // 2) For debugging
       window.source$ = interval(1000);
 
-      // TODO: wait for the appropriate client connections to start
-      //     await window.museClient.connect();
-      //     await window.museClient.start();
-      //     setStatus(generalTranslations.connected);
+      // TODO(kylemath) wait for the appropriate client connections to start
+      // TODO(keyfer): these awaits is why we need THIS funciton to be async
+      // await window.museClient.connect();
+      // await window.museClient.start();
+      // setStatus(generalTranslations.connected);
 
       // TODO: ensure that the client is connected and eegReadings are coming in
       // if (window.museClient && window.museClient.eegReadings) {
       if (window.source$) {
-        console.log('Successfully connected to createEEG Observable.')
+        console.log('Successfully connected to data source Observable.')
         setStatus(generalTranslations.connected);
 
         // Build the data source from the data source
         console.log('Build the data pipes from the data source.')
-        // TODO: need to zipSamples here
-        window.pipedDataRaw$ = window.source$.pipe(
+
+        // RAW DATA HANDLED HERE
+        // // TODO: need to zipSamples here
+        window.pipeRaw$ = window.source$.pipe(
           // TODO: implement the eeg operations here
           // bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
           // epoch({ duration: 1024, interval: 50, samplingRate: 256 }),
-          
-          // ensure we are catching errors correctly, and logging
           catchError(err => {
-            throw 'Error in pipedData. Details: ' + err;
+            console.log(err);
           }),
         );
 
+        // SPECTRA DATA HANDLED HERE
         // TODO: need to zipSamples here
-        window.pipedDataSpectra$ = window.source$.pipe(
+        window.pipeSpectra$ = window.source$.pipe(
           // TODO: implement the fft operations here
           // bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
           // epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
           // fft({ bins: 256 }),
           // sliceFFT([1, 30]),
-          // ensure we are catching errors correctly, and logging
           catchError(err => {
-            throw 'Error in pipedData. Details: ' + err;
+            console.log(err);
           }),
+        );
+
+        window.multiCastRaw$ = window.pipeRaw$.pipe(
+          multicast(() => new Subject())
+        );
+
+        window.multiCastSpectra$ = window.pipeSpectra$.pipe(
+          multicast(() => new Subject())
         );
 
         switch (selected) {
           case translations.types.spectra:
             // Subscribe to observable with spectra data view
             console.log('spectra view subscribed');
-            window.subscriptionSpectra$ = window.pipedDataSpectra$.subscribe(
+            window.subscriptionSpectra$ = window.multiCastSpectra$.subscribe(
               (v) => console.log('spectra view: ' + v),
               (err) => console.log(err)
             );
@@ -103,7 +113,7 @@ export function PageSwitcher() {
           case translations.types.raw:
             // Subscribe to observable with raw data view
             console.log('raw view subscribed');
-            window.subscriptionRaw$ = window.pipedDataRaw$.subscribe(
+            window.subscriptionRaw$ = window.multiCastRaw$.subscribe(
               (v) => console.log('raw view: ' + v)
             );
             break;
@@ -111,6 +121,10 @@ export function PageSwitcher() {
             console.log('Error on first subscription switch.')
           }
         } 
+        
+        window.multiCastRaw$.connect();
+        window.multiCastSpectra$.connect();
+
       } catch (err) {
         // Catch the connection error here.
         setStatus(generalTranslations.connect);
@@ -128,10 +142,13 @@ export function PageSwitcher() {
           console.log('Successfully unsubscribed from Spectra');
           
           // Resubscribe to observable with raw data view
-          window.subscriptionRaw$ = window.pipedDataRaw$.subscribe(
+          window.subscriptionRaw$ = window.multiCastRaw$.subscribe(
             (v) => console.log('raw view: ' + v)
           );
           console.log('Resubscribed to Raw');
+          
+          // Ensure that the raw is connected
+          window.multiCastRaw$.connect();
         }
         break;
       
@@ -142,11 +159,14 @@ export function PageSwitcher() {
           console.log('Successfully unsubscribed from Raw');
           
           // Subscribe to observable with spectra data view
-          window.subscriptionSpectra$ = window.pipedDataSpectra$.subscribe(
+          window.subscriptionSpectra$ = window.multiCastSpectra$.subscribe(
             (v) => console.log('spectra view: ' + v),
             (err) => console.log(err)
           );
           console.log('Resubscribed to Spectra');
+
+          // Ensure that the spectra is connected
+          window.multiCastSpectra$.connect();
         }
         break;
       
@@ -154,105 +174,6 @@ export function PageSwitcher() {
         console.log('Error on handleSubscriptions, switchToward: '+ switchToward);
     }
   }
-
-  // async function connect() {
-  //   window.museClient = new MuseClient();
-  //   setStatus(generalTranslations.connecting);
-
-  //   try {
-  //     await window.museClient.connect();
-  //     await window.museClient.start();
-  //     setStatus(generalTranslations.connected);
-
-  //     if (window.museClient && window.museClient.eegReadings) {
-  //       switch (selected) {
-  //         case translations.types.raw:
-  //           if (window.rawZippedData$ === null) {
-  //             window.rawZippedData$ = zipSamples(
-  //               window.museClient.eegReadings
-  //             ).pipe(
-  //               bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
-  //               epoch({ duration: 1024, interval: 50, samplingRate: 256 }),
-  //               catchError(err => {
-  //                 console.log(err);
-  //               })
-  //             );
-  //           }
-
-  //           window.rawSubscription$ = window.rawZippedData$.subscribe(data => {
-  //             setRawData(rawData => {
-  //               Object.values(rawData).forEach(
-  //                 (channel, index) => {
-  //                   if (index < 4) {
-  //                     const sRate = data.info.samplingRate;
-
-  //                     channel.datasets[0].data = data.data[index];
-  //                     channel.xLabels = customCount(
-  //                       (1000 / sRate) * data.data[2].length,
-  //                       1000 / sRate,
-  //                       -(1000 / sRate)
-  //                     ).map(function(each_element) {
-  //                       return Number(each_element.toFixed(0));
-  //                     });
-  //                   }
-  //                 },
-  //                 err => {
-  //                   console.log(err);
-  //                 }
-  //               );
-
-  //               return {
-  //                 ch0: rawData.ch0,
-  //                 ch1: rawData.ch1,
-  //                 ch2: rawData.ch2,
-  //                 ch3: rawData.ch3
-  //               };
-  //             });
-  //           });
-  //           break;
-
-  //         case translations.types.spectra:
-  //           if (window.spectraZippedData$ === null) {
-  //             window.spectraZippedData$ = zipSamples(
-  //               window.museClient.eegReadings
-  //             ).pipe(
-  //               bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
-  //               epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
-  //               fft({ bins: 256 }),
-  //               sliceFFT([1, 30]),
-  //               catchError(err => {
-  //                 console.log(err);
-  //               })
-  //             );
-  //           }
-
-  //           window.spectraSubscription$ = window.spectraZippedData$.subscribe(
-  //             data => {
-  //               setSpectraData(spectraData => {
-  //                 Object.values(spectraData).forEach((channel, index) => {
-  //                   if (index < 4) {
-  //                     channel.datasets[0].data = data.psd[index];
-  //                     channel.xLabels = data.freqs;
-  //                   }
-  //                 });
-
-  //                 return {
-  //                   ch0: spectraData.ch0,
-  //                   ch1: spectraData.ch1,
-  //                   ch2: spectraData.ch2,
-  //                   ch3: spectraData.ch3
-  //                 };
-  //               });
-  //             }
-  //           );
-  //           break;
-  //       }
-  //     }
-  //   } catch (err) {
-  //     setStatus(generalTranslations.connect);
-  //     console.log(err);
-  //   }
-  // }
 
   return (
     <React.Fragment>
