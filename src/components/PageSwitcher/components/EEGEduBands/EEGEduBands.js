eece3494cee97b62ff8fb7d1b12cededf68d0957
Kyle Mathewson <kylemath@gmail.com>
2019-12-10 20:09:39 -0700

    added fband module

---
+++ src/components/PageSwitcher/components/EEGEduBands/EEGEduBands.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import { Card, Stack, TextContainer } from "@shopify/polaris";
+
+import { channelNames } from "muse-js";
+import { Bar } from "react-chartjs-2";
+
+import { chartStyles, generalOptions } from "../chartOptions";
+
+import * as generalTranslations from "../translations/en";
+import * as specificTranslations from "./translations/en";
+
+export default function EEGEduBands(channels) {
+  function renderCharts() {
+    return Object.values(channels.data).map((channel, index) => {
+      const options = {
+        ...generalOptions,
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                ...generalOptions.scales.xAxes[0].scaleLabel,
+                labelString: specificTranslations.xlabel
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                ...generalOptions.scales.yAxes[0].scaleLabel,
+                labelString: specificTranslations.ylabel
+              },
+              ticks: {
+                max: 50,
+                min: 0
+              }
+            }
+          ]
+        },
+        title: {
+          ...generalOptions.title,
+          text: generalTranslations.channel + channelNames[index]
+        }
+      };
+
+      return (
+        <Card.Section key={"Card_" + index}>
+          <Bar key={"Line_" + index} data={channel} options={options} />
+        </Card.Section>
+      );
+    });
+  }
+
+  return (
+    <Card title={specificTranslations.title}>
+      <Card.Section>
+        <Stack>
+          <TextContainer>
+            <p>{specificTranslations.description}</p>
+          </TextContainer>
+        </Stack>
+      </Card.Section>
+      <Card.Section>
+        <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
+      </Card.Section>
+    </Card>
+  );
+}
