07695486964994721e037fb6dc91c88400d47cf4
Kory Mathewson <korymath@gmail.com>
2019-12-12 09:41:20 -0800

    adding mockMuseEEG data stream, flag to toggle

---
+++ src/components/PageSwitcher/utils/mockMuseEEG.js
@@ -0,0 +1,28 @@
+const { interval, from } = require('rxjs');
+const { map, flatMap } = require('rxjs/operators');
+
+const samples = () => {
+ return Array(12)
+  .fill()
+  .map(_ => Math.random())
+};
+
+const transform = (index) => {
+ const timestamp = Date.now();
+ return from([0,1,2,3]).pipe(
+  map(electrode => ({
+   timestamp,
+   electrode,
+   index,
+   samples: samples()
+  }))
+ )
+};
+
+export const mockMuseEEG = (sampleRate) => {
+ let index = 0;
+ return interval(1000 / sampleRate).pipe(
+  map(() => index += 1),
+  flatMap(transform),
+ )
+};
