1404e4de82cb58a7770f30086ecd30a6f6540f03
Kyle Mathewson <kylemath@gmail.com>
2019-12-15 13:28:32 -0700

    moving into modules

--- src/components/PageSwitcher/components/EEGEduBands/EEGEduBands.js
+++ src/components/PageSwitcher/components/EEGEduBands/EEGEduBands.js
@@ -1,16 +1,28 @@
 import React from "react";
+import { catchError, multicast } from "rxjs/operators";
 
 import { Card, Stack, TextContainer } from "@shopify/polaris";
+import { Subject } from "rxjs";
 
 import { channelNames } from "muse-js";
 import { Bar } from "react-chartjs-2";
+
+import { zipSamples } from "muse-js";
+
+import {
+  bandpassFilter,
+  epoch,
+  fft,
+  powerByBand
+} from "@neurosity/pipes";
 
 import { chartStyles, generalOptions } from "../chartOptions";
 
 import * as generalTranslations from "../translations/en";
 import * as specificTranslations from "./translations/en";
+import { numOptions, bandLabels } from "../../utils/chartUtils";
 
-export default function EEGEduBands(channels) {
+export function EEGEduBands(channels) {
   function renderCharts() {
     return Object.values(channels.data).map((channel, index) => {
       const options = {
@@ -64,5 +76,59 @@ export default function EEGEduBands(channels) {
         <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
       </Card.Section>
     </Card>
+  );
+}
+
+
+export function setupBands(setBandsData) {
+  console.log("Subscribing to Bands");
+
+  if (window.multicastBands$) {
+    window.subscriptionBands$ = window.multicastBands$.subscribe(data => {
+      setBandsData(bandsData => {
+        Object.values(bandsData).forEach((channel, index) => {
+          if (index < 4) {
+            channel.datasets[0].data = [
+              data.delta[index],
+              data.theta[index],
+              data.alpha[index],
+              data.beta[index],
+              data.gamma[index]
+            ];
+            channel.xLabels = bandLabels;
+          }
+        });
+
+        return {
+          ch0: bandsData.ch0,
+          ch1: bandsData.ch1,
+          ch2: bandsData.ch2,
+          ch3: bandsData.ch3
+        };
+      });
+    });
+
+    window.multicastBands$.connect();
+    console.log("Subscribed to Bands");
+  }
+}
+
+export function buildPipeBands() {
+  // Build Pipe Bands
+  window.pipeBands$ = zipSamples(window.source$.eegReadings).pipe(
+    bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+    epoch({
+      duration: numOptions.duration,
+      interval: 100,
+      samplingRate: numOptions.srate
+    }),
+    fft({ bins: 256 }),
+    powerByBand(),
+    catchError(err => {
+      console.log(err);
+    })
+  );
+  window.multicastBands$ = window.pipeBands$.pipe(
+    multicast(() => new Subject())
   );
 }
--- src/components/PageSwitcher/components/EEGEduSpectra/EEGEduSpectra.js
+++ src/components/PageSwitcher/components/EEGEduSpectra/EEGEduSpectra.js
@@ -1,16 +1,28 @@
 import React from "react";
+import { catchError, multicast } from "rxjs/operators";
 
 import { Card, Stack, TextContainer } from "@shopify/polaris";
+import { Subject } from "rxjs";
 
 import { channelNames } from "muse-js";
 import { Line } from "react-chartjs-2";
+
+import { zipSamples } from "muse-js";
+
+import {
+  bandpassFilter,
+  epoch,
+  fft,
+  sliceFFT
+} from "@neurosity/pipes";
 
 import { chartStyles, generalOptions } from "../chartOptions";
 
 import * as generalTranslations from "../translations/en";
 import * as specificTranslations from "./translations/en";
+import { numOptions } from "../../utils/chartUtils";
 
-export default function EEGEduSpectra(channels) {
+export function EEGEduSpectra(channels) {
   function renderCharts() {
     return Object.values(channels.data).map((channel, index) => {
       const options = {
@@ -64,5 +76,52 @@ export default function EEGEduSpectra(channels) {
         <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
       </Card.Section>
     </Card>
+  );
+}
+
+export function setupSpectra(setSpectraData) {
+  console.log("Subscribing to Spectra");
+
+  if (window.multicastSpectra$) {
+    window.subscriptionSpectra$ = window.multicastSpectra$.subscribe(data => {
+      setSpectraData(spectraData => {
+        Object.values(spectraData).forEach((channel, index) => {
+          if (index < 4) {
+            channel.datasets[0].data = data.psd[index];
+            channel.xLabels = data.freqs;
+          }
+        });
+
+        return {
+          ch0: spectraData.ch0,
+          ch1: spectraData.ch1,
+          ch2: spectraData.ch2,
+          ch3: spectraData.ch3
+        };
+      });
+    });
+
+    window.multicastSpectra$.connect();
+    console.log("Subscribed to Spectra");
+  }
+}
+
+export function buildPipeSpectra () {
+  // Build Pipe Spectra
+  window.pipeSpectra$ = zipSamples(window.source$.eegReadings).pipe(
+    bandpassFilter({ cutoffFrequencies: [2, 20], nbChannels: 4 }),
+    epoch({
+      duration: numOptions.duration,
+      interval: 100,
+      samplingRate: numOptions.srate
+    }),
+    fft({ bins: 256 }),
+    sliceFFT([1, 30]),
+    catchError(err => {
+      console.log(err);
+    })
+  );
+  window.multicastSpectra$ = window.pipeSpectra$.pipe(
+    multicast(() => new Subject())
   );
 }
