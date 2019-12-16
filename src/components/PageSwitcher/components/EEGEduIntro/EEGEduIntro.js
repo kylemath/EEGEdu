d39f2566cd0c2e73b69cef572a33e77fb255fe32
Kyle Mathewson <kylemath@gmail.com>
2019-12-16 11:49:48 -0700

    created intro module copy of raw

---
+++ src/components/PageSwitcher/components/EEGEduIntro/EEGEduIntro.js
@@ -0,0 +1,218 @@
+import React from "react";
+import { catchError, multicast } from "rxjs/operators";
+import { Subject } from "rxjs";
+
+import { TextContainer, Card, Stack, RangeSlider } from "@shopify/polaris";
+
+import { channelNames } from "muse-js";
+import { Line } from "react-chartjs-2";
+
+import { zipSamples } from "muse-js";
+
+import {
+  bandpassFilter,
+  epoch
+} from "@neurosity/pipes";
+
+import { chartStyles, generalOptions } from "../chartOptions";
+
+import * as generalTranslations from "../translations/en";
+import * as specificTranslations from "./translations/en";
+
+import { generateXTics, standardDeviation } from "../../utils/chartUtils";
+
+export function getSettings () {
+  return {
+    cutOffLow: 2,
+    cutOffHigh: 20,
+    nbChannels: 4,
+    interval: 50,
+    srate: 256,
+    duration: 1024
+  }
+};
+
+export function buildPipe(Settings) {
+  if (window.subscriptionRaw$) window.subscriptionRaw$.unsubscribe();
+
+  window.pipeRaw$ = null;
+  window.multicastRaw$ = null;
+  window.subscriptionRaw$ = null;
+
+  // Build Pipe Raw
+  window.pipeRaw$ = zipSamples(window.source$.eegReadings).pipe(
+    bandpassFilter({ 
+      cutoffFrequencies: [Settings.cutOffLow, Settings.cutOffHigh], 
+      nbChannels: Settings.nbChannels }),
+    epoch({
+      duration: Settings.duration,
+      interval: Settings.interval,
+      samplingRate: Settings.srate
+    }),
+    catchError(err => {
+      console.log(err);
+    })
+  );
+  window.multicastRaw$ = window.pipeRaw$.pipe(
+    multicast(() => new Subject())
+  );
+}
+
+export function setup(setData, Settings) {
+  console.log("Subscribing to Raw");
+
+  if (window.multicastRaw$) {
+    window.subscriptionRaw$ = window.multicastRaw$.subscribe(data => {
+      setData(rawData => {
+        Object.values(rawData).forEach((channel, index) => {
+          if (index < 4) {
+            channel.datasets[0].data = data.data[index];
+            channel.xLabels = generateXTics(Settings.srate, Settings.duration);
+            channel.datasets[0].qual = standardDeviation(data.data[index])          
+          }
+        });
+
+        return {
+          ch0: rawData.ch0,
+          ch1: rawData.ch1,
+          ch2: rawData.ch2,
+          ch3: rawData.ch3
+        };
+      });
+    });
+
+    window.multicastRaw$.connect();
+    console.log("Subscribed to Raw");
+  }
+}
+
+export function EEGEdu(channels) {
+  function renderCharts() {
+    return Object.values(channels.data).map((channel, index) => {
+      const options = {
+        ...generalOptions,
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                ...generalOptions.scales.xAxes[0].scaleLabel,
+                labelString: specificTranslations.xlabel
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                ...generalOptions.scales.yAxes[0].scaleLabel,
+                labelString: specificTranslations.ylabel
+              },
+              ticks: {
+                max: 300,
+                min: -300
+              }
+            }
+          ]
+        },
+        elements: {
+          line: {
+            borderColor: 'rgba(' + channel.datasets[0].qual*10 + ', 128, 128)',
+            fill: false
+          },
+          point: {
+            radius: 0
+          }
+        },
+        animation: {
+          duration: 0
+        },
+        title: {
+          ...generalOptions.title,
+          text: generalTranslations.channel + channelNames[index] + ' --- SD: ' + channel.datasets[0].qual 
+        }
+      };
+
+      return (
+        <Card.Section key={"Card_" + index}>
+          <Line key={"Line_" + index} data={channel} options={options} />
+        </Card.Section>
+      );
+    });
+  }
+
+  return (
+    <Card title={specificTranslations.title}>
+      <Card.Section>
+        <Stack>
+          <TextContainer>
+            <p>{specificTranslations.description}</p>
+          </TextContainer>
+        </Stack>
+      </Card.Section>
+      <Card.Section>
+        <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
+      </Card.Section>
+    </Card>
+  );
+}
+
+  
+export function renderSliders(setData, setSettings, status, Settings) {
+
+  function handleIntervalRangeSliderChange(value) {
+    setSettings(prevState => ({...prevState, interval: value}));
+    buildPipe(Settings);
+    setup(setData, Settings);
+  }
+
+  function handleCutoffLowRangeSliderChange(value) {
+    setSettings(prevState => ({...prevState, cutOffLow: value}));
+    buildPipe(Settings);
+    setup(setData, Settings);
+  }
+
+  function handleCutoffHighRangeSliderChange(value) {
+    setSettings(prevState => ({...prevState, cutOffHigh: value}));
+    buildPipe(Settings);
+    setup(setData, Settings);
+  }
+
+  function handleDurationRangeSliderChange(value) {
+    setSettings(prevState => ({...prevState, duration: value}));
+    buildPipe(Settings);
+    setup(setData, Settings);
+  }
+
+
+  return (
+    <React.Fragment>
+      <RangeSlider 
+        disabled={status === generalTranslations.connect} 
+        min={128} step={128} max={4096}
+        label={'Epoch duration (Sampling Points): ' + Settings.duration} 
+        value={Settings.duration} 
+        onChange={handleDurationRangeSliderChange} 
+      />          
+      <RangeSlider 
+        disabled={status === generalTranslations.connect} 
+        min={10} step={5} max={Settings.duration}
+        label={'Sampling points between epochs onsets: ' + Settings.interval} 
+        value={Settings.interval} 
+        onChange={handleIntervalRangeSliderChange} 
+      />
+      <RangeSlider 
+        disabled={status === generalTranslations.connect} 
+        min={.01} step={.5} max={Settings.cutOffHigh - .5}
+        label={'Cutoff Frequency Low: ' + Settings.cutOffLow + ' Hz'} 
+        value={Settings.cutOffLow} 
+        onChange={handleCutoffLowRangeSliderChange} 
+      />
+      <RangeSlider 
+        disabled={status === generalTranslations.connect} 
+        min={Settings.cutOffLow + .5} step={.5} max={Settings.srate/2}
+        label={'Cutoff Frequency High: ' + Settings.cutOffHigh + ' Hz'} 
+        value={Settings.cutOffHigh} 
+        onChange={handleCutoffHighRangeSliderChange} 
+      />
+    </React.Fragment>
+  )
+}
