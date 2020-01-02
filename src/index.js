aff1112fae1f3a446dcfa3d16472b0d9555a1cc5
Kyle Mathewson <kylemath@gmail.com>
2020-01-02 14:35:50 -0700

    trying to stop cache of website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App/App";
-import registerServiceWorker from "./workers/registerServiceWorker";
+// import registerServiceWorker from "./workers/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
