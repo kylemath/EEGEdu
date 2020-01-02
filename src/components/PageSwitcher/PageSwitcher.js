b6896490e20e0bfa91366774d9938a550210d22a
Kyle Mathewson <kylemath@gmail.com>
2020-01-02 08:05:07 -0700

    back to normal settings for ready to deploy

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -47,7 +47,7 @@ export function PageSwitcher() {
   const [status, setStatus] = useState(generalTranslations.connect);
 
   // for picking a new module
-  const [selected, setSelected] = useState(alpha);
+  const [selected, setSelected] = useState(intro);
   const handleSelectChange = useCallback(value => {
     setSelected(value);
 
