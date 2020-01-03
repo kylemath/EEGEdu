d86aa15ef536084899cd5b4a51118ca58b5fdeb9
System Administrator <kylemath@gmail.com>
2020-01-02 23:34:32 -0700

    back to intro

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -51,7 +51,7 @@ export function PageSwitcher() {
   const [status, setStatus] = useState(generalTranslations.connect);
 
   // for picking a new module
-  const [selected, setSelected] = useState(ssvep);
+  const [selected, setSelected] = useState(intro);
   const handleSelectChange = useCallback(value => {
     setSelected(value);
 
