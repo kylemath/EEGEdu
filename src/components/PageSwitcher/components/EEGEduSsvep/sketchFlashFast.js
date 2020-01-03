86748d882c9d187127a382bab66f04e30444595b
System Administrator <kylemath@gmail.com>
2020-01-02 23:18:08 -0700

    p5js flashing disk in modal windows at two different frequencies

---
+++ src/components/PageSwitcher/components/EEGEduSsvep/sketchFlashFast.js
@@ -0,0 +1,30 @@
+export default function sketchFlash (p) {
+
+  let x = 0;
+
+  p.setup = function () {
+    p.createCanvas(500, 500);
+  };
+
+  p.windowResized = function() {
+    p.createCanvas(500, 500);
+  }
+
+  p.mousePressed = function () {
+    p.background(256);
+  }
+
+  p.draw = function () {
+    p.background(255);
+    p.fill(255, 255, 255);
+    p.text("+", p.width/2, p.height/2);
+    x = x+1;
+    if (x % 5 === 0) {
+      p.fill(0, 0, 0);  
+    } else {
+      p.fill(255, 255, 255);
+    }
+    p.noStroke();
+    p.ellipse(p.width/2, p.height/2, 500);
+  }
+};
