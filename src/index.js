981530ab82ae0c28655e81bab04019f86255ae91
Kory Mathewson <korymath@gmail.com>
2020-01-03 16:55:50 -0500

    removing service worker as it is no longer used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App/App";
-// import registerServiceWorker from "./workers/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-// registerServiceWorker();
