aff9a9ea6c1f9a95d2e707fd4669cecf29127299
Kyle Mathewson <kylemath@gmail.com>
2020-01-06 01:39:57 -0700

    removed unnecessary login in the pipes since it only does what is present in data from muse-js kept login in charts

--- src/components/PageSwitcher/components/EEGEduRaw/EEGEduRaw.js
+++ src/components/PageSwitcher/components/EEGEduRaw/EEGEduRaw.js
@@ -63,35 +63,24 @@ export function buildPipe(Settings) {
 
 export function setup(setData, Settings) {
   console.log("Subscribing to " + Settings.name);
-
   if (window.multicastRaw$) {
     window.subscriptionRaw = window.multicastRaw$.subscribe(data => {
       setData(rawData => {
-
         Object.values(rawData).forEach((channel, index) => {
-          if (index < window.nchans) {
-            channel.datasets[0].data = data.data[index];
-            channel.xLabels = generateXTics(Settings.srate, Settings.duration);
-            channel.datasets[0].qual = standardDeviation(data.data[index])          
-          }
+          channel.datasets[0].data = data.data[index];
+          channel.xLabels = generateXTics(Settings.srate, Settings.duration);
+          channel.datasets[0].qual = standardDeviation(data.data[index])          
         });
 
-        if (window.enableAux) {
-          return {
-            ch0: rawData.ch0,
-            ch1: rawData.ch1,
-            ch2: rawData.ch2,
-            ch3: rawData.ch3,
-            ch4: rawData.ch4
-          };
-        } else {
-          return {
-            ch0: rawData.ch0,
-            ch1: rawData.ch1,
-            ch2: rawData.ch2,
-            ch3: rawData.ch3
-          }
-        }
+
+        return {
+          ch0: rawData.ch0,
+          ch1: rawData.ch1,
+          ch2: rawData.ch2,
+          ch3: rawData.ch3,
+          ch4: rawData.ch4
+        };
+     
       });
     });
 
@@ -104,56 +93,57 @@ export function renderModule(channels) {
   function renderCharts() {
     return Object.values(channels.data).map((channel, index) => {
       if (index < window.nchans) {
-      const options = {
-        ...generalOptions,
-        scales: {
-          xAxes: [
-            {
-              scaleLabel: {
-                ...generalOptions.scales.xAxes[0].scaleLabel,
-                labelString: specificTranslations.xlabel
+        const options = {
+          ...generalOptions,
+          scales: {
+            xAxes: [
+              {
+                scaleLabel: {
+                  ...generalOptions.scales.xAxes[0].scaleLabel,
+                  labelString: specificTranslations.xlabel
+                }
               }
-            }
-          ],
-          yAxes: [
-            {
-              scaleLabel: {
-                ...generalOptions.scales.yAxes[0].scaleLabel,
-                labelString: specificTranslations.ylabel
-              },
-              ticks: {
-                max: 300,
-                min: -300
+            ],
+            yAxes: [
+              {
+                scaleLabel: {
+                  ...generalOptions.scales.yAxes[0].scaleLabel,
+                  labelString: specificTranslations.ylabel
+                },
+                ticks: {
+                  max: 300,
+                  min: -300
+                }
               }
-            }
-          ]
-        },
-        elements: {
-          line: {
-            borderColor: 'rgba(' + channel.datasets[0].qual*10 + ', 128, 128)',
-            fill: false
+            ]
           },
-          point: {
-            radius: 0
+          elements: {
+            line: {
+              borderColor: 'rgba(' + channel.datasets[0].qual*10 + ', 128, 128)',
+              fill: false
+            },
+            point: {
+              radius: 0
+            }
+          },
+          animation: {
+            duration: 0
+          },
+          title: {
+            ...generalOptions.title,
+            text: generalTranslations.channel + channelNames[index] + ' --- SD: ' + channel.datasets[0].qual 
           }
-        },
-        animation: {
-          duration: 0
-        },
-        title: {
-          ...generalOptions.title,
-          text: generalTranslations.channel + channelNames[index] + ' --- SD: ' + channel.datasets[0].qual 
-        }
-      };
+        };
 
-      return (
-        <Card.Section key={"Card_" + index}>
-          <Line key={"Line_" + index} data={channel} options={options} />
-        </Card.Section>
-      );
-    } else {
-      return null
-    }
+        return (
+          <Card.Section key={"Card_" + index}>
+            <Line key={"Line_" + index} data={channel} options={options} />
+          </Card.Section>
+        );
+      } else {
+        return null
+      }
+
     });
   }
 
