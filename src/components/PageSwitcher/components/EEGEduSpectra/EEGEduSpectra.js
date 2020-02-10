42caaa9cf83f56a4bdfbf31753980645fdaf254d
Kyle Mathewson <kylemath@gmail.com>
2020-02-10 14:36:00 -0700

    skeleton of new module

--- src/components/PageSwitcher/components/EEGEduSpectra/EEGEduSpectra.js
+++ src/components/PageSwitcher/components/EEGEduSpectra/EEGEduSpectra.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { catchError, multicast } from "rxjs/operators";
 
-import { TextContainer, Card, Stack, RangeSlider, Button, ButtonGroup, Modal } from "@shopify/polaris";
+import { TextContainer, Card, Stack, RangeSlider, Button, ButtonGroup, Modal, Link } from "@shopify/polaris";
 import { saveAs } from 'file-saver';
 import { take, takeUntil } from "rxjs/operators";
 import { Subject, timer } from "rxjs";
 
 import { channelNames } from "muse-js";
 import { Line } from "react-chartjs-2";
+import YouTube from 'react-youtube'
 
 import { zipSamples } from "muse-js";
 
@@ -185,19 +186,92 @@ export function renderModule(channels) {
 
   }
 
+
+  const opts = {
+    height: '195',
+    width: '320',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: false
+    }
+  };
+
   return (
-    <Card title={specificTranslations.title}>
-      <Card.Section>
-        <Stack>
-          <TextContainer>
-            <p>{specificTranslations.description}</p>
-          </TextContainer>
-        </Stack>
-      </Card.Section>
-      <Card.Section>
-        <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
-      </Card.Section>
-    </Card>
+    <React.Fragment>
+      <Card title={specificTranslations.title}>
+        <Card.Section>
+          <Stack>
+            <TextContainer>
+              <p>{specificTranslations.description}</p>
+            </TextContainer>
+          </Stack>
+        </Card.Section>
+        <Card.Section>
+          <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
+        </Card.Section>
+      </Card>
+      <Card title={'Fourier Transform'}>
+        <Card.Section>
+          <Stack>
+            <TextContainer>
+              <p> {[
+                "In module 3 we saw how we can use mathematical techniques such as a fourier transform to estimate what frequency is present in the ECG data. ",
+                "A fourier transform turns any series of numbers into a summed set of sine waves of different sizes. ",
+                "The following animation shows how a single time-series of data, can be thought of as the sum of different frequencies of sine waves, each of a different magnitude. ", 
+                "The blue line chart in the animation shows what is called the spectra, and indicates the power at each frequency." 
+              ]} </p>
+            </TextContainer>
+            <br />
+            <img 
+              src={ require("./fft_animation.gif")} 
+              alt="FFT"
+              width="50%"
+              height="auto"
+            ></img> 
+            </Stack>
+            <Stack> 
+            <Link url="https://en.wikipedia.org/wiki/Electrocardiography#/media/File:Limb_leads_of_EKG.png"
+              external={true}>
+              Image Source - Wikipedia </Link>
+              <br />
+              <TextContainer>
+              <p> {[
+                "The Fourier transform is a mathematical technique originally developed in the early 1800s. ",
+               "In module 3 we saw how we can use mathematical techniques such as a fourier transform to estimate what frequency is present in the ECG data. ",
+                "A fourier transform turns any series of numbers into a summed set of sine waves of different sizes. ",
+                "The following animation shows how a single time-series of data, can be thought of as the sum of different frequencies of sine waves, each of a different magnitude. ", 
+                            "In module 3 we saw how we can use mathematical techniques such as a fourier transform to estimate what frequency is present in the ECG data. ",
+                "A fourier transform turns any series of numbers into a summed set of sine waves of different sizes. ",
+                "The following animation shows how a single time-series of data, can be thought of as the sum of different frequencies of sine waves, each of a different magnitude. ", 
+                             "Something. " 
+                ]} 
+           <Link url="http://www.jezzamon.com/fourier/index.html"
+              external={true}>
+              Follow this link to an excellent interactive tutorial on fourier transforms using sound
+             </Link>
+                </p>
+             <p> {[
+                "In module 3 we saw how we can use mathematical techniques such as a fourier transform to estimate what frequency is present in the ECG data. ",
+                "A fourier transform turns any series of numbers into a summed set of sine waves of different sizes. ",
+                "The following animation shows how a single time-series of data, can be thought of as the sum of different frequencies of sine waves, each of a different magnitude. ", 
+                "The blue line chart in the animation shows what is called the spectra, and indicates the power at each frequency." 
+              ]} </p>
+            </TextContainer>
+
+            <YouTube 
+              videoId="NAsM30MAHLg"
+              opts={opts}
+            />
+             <p> {[
+                "In module 3 we saw how we can use mathematical techniques such as a fourier transform to estimate what frequency is present in the ECG data. ",
+                "A fourier transform turns any series of numbers into a summed set of sine waves of different sizes. ",
+                "The following animation shows how a single time-series of data, can be thought of as the sum of different frequencies of sine waves, each of a different magnitude. ", 
+                "The blue line chart in the animation shows what is called the spectra, and indicates the power at each frequency." 
+              ]} </p>
+
+          </Stack>
+        </Card.Section>
+      </Card>
+    </React.Fragment>
   );
 }
 
@@ -292,9 +366,25 @@ export function renderRecord(recordPopChange, recordPop, status, Settings, setSe
     setSettings(prevState => ({...prevState, secondsToSave: value}));
   }
  
+  const opts = {
+    height: '195',
+    width: '320',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: false
+    }
+  };
+ 
   return(
     <Card title={'Record ' + Settings.name +' Data'} sectioned>
       <Stack>
+         <TextContainer>
+          <p> {[
+            "In module 3 we saw how we can use mathematical techniques such as a fourier transform to estimate what frequency is present in the ECG data. ",
+            "A fourier transform turns any series of numbers into a summed set of sine waves of different sizes. ",
+            "The following animation shows how a single time-series of data, can be thought of as the sum of different frequencies of sine waves, each of a different magnitude. ", 
+            "The blue line chart in the animation shows what is called the spectra, and indicates the power at each frequency." 
+          ]} </p>
+        </TextContainer>
         <RangeSlider 
           disabled={status === generalTranslations.connect} 
           min={2}
@@ -315,6 +405,47 @@ export function renderRecord(recordPopChange, recordPop, status, Settings, setSe
             {'Save to CSV'}  
           </Button>
         </ButtonGroup>
+   <TextContainer>
+          <p> {[
+            "A .csv file will be saved that can be opened in Google Sheets. ",
+            "Remember for each person to record two files, one while  standing and one while  sitting. ", 
+            "Here is an example of what the data will look like once loaded. ",
+            "The first column shows the time in msec of each estimate of heart rate. The second column shows the heart rate estimate in BPM. ",
+            "Each row represents an estimate from the previous 8 seconds of data. ",
+            "This is because you need a long segment of time to estimate frequency of rhythms over time. ",
+            "Each subsequent row is from an 8 second chunk of data, but is taken about 400 ms after the previous row. ",
+            "You can see the time values increase about 10000 ms during the recording, representing the 10 seconds of data. ",
+            "So 10000 milliseconds divided into ~400 ms shifts per row gives us the rough number of rows (~25). ", 
+            "The graph shows the values plotted over time, showing that the 1st Harmonic value of 120 BPM was incorrectly recorded on a few windows. "
+          ]} </p>
+        </TextContainer>
+        <img 
+          src={ require("./exampleOutput.png")} 
+          alt="exampleOutput"
+          width="75%"
+          height="auto"
+        ></img>
+        <TextContainer>
+          <p> {[
+            "In this module the analysis will be much easier, since much of the work has been done by the webpage with the fourier transform and peak detection. ",
+            "The following youtube video will show you how to open the file in Google Sheets, rename it, plot the data, remove any harmonics or outliers,  ",
+            "then take the average heart rate over the window as an estimate of your heart rate. "
+            ]} 
+        <Link url="https://docs.google.com/spreadsheets/d/1AcFxuIZDMNfOifgql2vrFy2nSS3g7c14fOxAdptOgQY/edit?usp=sharing"
+             external={true}>
+
+         Link to example google sheet from video. 
+        </Link>  
+        </p>
+        </TextContainer>
+        <br />
+        <YouTube 
+          videoId="6EtzwGXjB9Q"
+          opts={opts}
+        />
+
+
+
         <Modal
           open={recordPop}
           onClose={recordPopChange}
