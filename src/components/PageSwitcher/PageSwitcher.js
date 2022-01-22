4ca4f42028f63fcad190f1afbe88203eb724a616
Keyfer Mathewson <keyfer.mathewson@clio.com>
2022-01-22 16:51:14 -0700

    Make it work

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -1,101 +1,14 @@
 import React, { useState } from "react";
-import { MuseClient } from "muse-js";
 import { Card, Stack, Button, ButtonGroup } from "@shopify/polaris";
-
-import { mockMuseEEG } from "./utils/mockMuseEEG";
+import { Animate } from "./components/EEGEduAnimate/EEGEduAnimate";
 
 import * as generalTranslations from "./components/translations/en";
-import { emptyChannelData } from "./components/chartOptions";
-
-import {
-  settings as animateSettings,
-  Animate,
-} from "./components/EEGEduAnimate/EEGEduAnimate";
-
-// import { messageService, subscriber } from './utils/messageService';
-
-import { zipSamples } from "muse-js";
-
-import { multicast } from "rxjs/operators";
-import { Subject } from "rxjs";
-
-import { bandpassFilter, epoch, fft, powerByBand } from "@neurosity/pipes";
 
 export function PageSwitcher() {
-  const [channelData, setChannelData] = useState(emptyChannelData);
   const [connectionStatus, setConnectionStatus] = useState({
     connected: false,
     type: undefined,
   });
-
-  async function connect(connectionType) {
-    let isConnected = false;
-
-    if (connectionType === "mock") {
-      window.source = {};
-      window.source.eegReadings$ = mockMuseEEG(256);
-
-      isConnected = true;
-      setConnectionStatus({ connected: true, type: "mock" });
-    } else {
-      window.source = new MuseClient();
-      await window.source.connect();
-      await window.source.start();
-      window.source.eegReadings$ = window.source.eegReadings;
-
-      isConnected = true;
-      setConnectionStatus({ connected: true, type: "muse" });
-    }
-
-    if (isConnected && window.source.eegReadings$) {
-      window.pipeBands$ = zipSamples(window.source.eegReadings$).pipe(
-        bandpassFilter({
-          cutoffFrequencies: [
-            animateSettings.cutOffLow,
-            animateSettings.cutOffHigh,
-          ],
-          nbChannels: animateSettings.nbChannels,
-        }),
-        epoch({
-          duration: animateSettings.duration,
-          interval: animateSettings.interval,
-          samplingRate: animateSettings.srate,
-        }),
-        fft({ bins: animateSettings.bins }),
-        powerByBand()
-      );
-      window.multicastBands$ = window.pipeBands$.pipe(
-        multicast(() => new Subject())
-      );
-
-      if (window.multicastBands$) {
-        window.multicastBands$.subscribe((data) => {
-          setChannelData((bandsData) => {
-            Object.values(bandsData).forEach((channel, index) => {
-              if (index < 4) {
-                channel.datasets[0].data = [
-                  data.delta[index],
-                  data.theta[index],
-                  data.alpha[index],
-                  data.beta[index],
-                  data.gamma[index],
-                ];
-              }
-            });
-
-            return {
-              ch0: bandsData.ch0,
-              ch1: bandsData.ch1,
-              ch2: bandsData.ch2,
-              ch3: bandsData.ch3,
-            };
-          });
-        });
-
-        window.multicastBands$.connect();
-      }
-    }
-  }
 
   return (
     <React.Fragment>
@@ -106,7 +19,7 @@ export function PageSwitcher() {
               primary
               disabled={connectionStatus.connected}
               onClick={() => {
-                connect("muse");
+                setConnectionStatus({ connected: true, type: "muse" });
               }}
             >
               {connectionStatus.connected && connectionStatus.type === "muse"
@@ -117,7 +30,7 @@ export function PageSwitcher() {
               primary
               disabled={connectionStatus.connected}
               onClick={() => {
-                connect("mock");
+                setConnectionStatus({ connected: true, type: "mock" });
               }}
             >
               {connectionStatus.connected && connectionStatus.type === "mock"
@@ -135,8 +48,7 @@ export function PageSwitcher() {
           </ButtonGroup>
         </Stack>
       </Card>
-
-      <Animate data={channelData} />
+      <Animate status={connectionStatus} />
     </React.Fragment>
   );
 }
--- src/components/PageSwitcher/components/EEGEduAnimate/EEGEduAnimate.js
+++ src/components/PageSwitcher/components/EEGEduAnimate/EEGEduAnimate.js
@@ -1,6 +1,12 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useRef, useCallback } from "react";
 
-import { Card, Button, ButtonGroup } from "@shopify/polaris";
+import { Card } from "@shopify/polaris";
+import { zipSamples } from "muse-js";
+import { bandpassFilter, epoch, fft, powerByBand } from "@neurosity/pipes";
+import { catchError, multicast } from "rxjs/operators";
+import { Subject } from "rxjs";
+
+import { mockMuseEEG } from "../../utils/mockMuseEEG";
 
 import { chartStyles } from "../chartOptions";
 
@@ -11,7 +17,7 @@ import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 
 import theme from "./p5Theme";
 
-export const settings = {
+const animateSettings = {
   cutOffLow: 2,
   cutOffHigh: 20,
   nbChannels: 4,
@@ -21,149 +27,119 @@ export const settings = {
   srate: 256,
 };
 
-export function Animate(channels) {
-  function RenderCharts() {
-    const [sketchPop, setSketchPop] = useState(false);
-    const sketchPopChange = useCallback(() => setSketchPop(!sketchPop), [
-      sketchPop,
-    ]);
+export function Animate(connection) {
+  const brain = useRef({
+    delta: 0,
+    theta: 0,
+    alpha: 0,
+    beta: 0,
+    gamma: 0,
+    textMsg: "No data.",
+  });
 
-    window.headerProps = {
-      delta: 0,
-      theta: 0,
-      alpha: 0,
-      beta: 0,
-      gamma: 0,
-      textMsg: "No data.",
-    };
+  let channelData$;
+  let pipeBands$;
+  let multicastBands$;
 
-    return Object.values(channels.data).map((channel, index) => {
-      //only left frontal channel
-      if (index === 1) {
-        if (channel.datasets[0].data) {
-          window.delta = channel.datasets[0].data[0];
-          window.theta = channel.datasets[0].data[1];
-          window.alpha = channel.datasets[0].data[2];
-          window.beta = channel.datasets[0].data[3];
-          window.gamma = channel.datasets[0].data[4];
+  if (connection.status.connected) {
+    if (connection.status.type === "mock") {
+      channelData$ = mockMuseEEG(256);
 
-          window.headerProps = {
-            delta: 10 * window.delta,
-            theta: 10 * window.theta,
-            alpha: 10 * window.alpha,
-            beta: 10 * window.beta,
-            gamma: 10 * window.gamma,
-            textMsg: "Data Recieved.",
-          };
-        }
-        const brain = window.headerProps;
-        const scope = { styled, brain, React, Sketch };
-        const code = `class MySketch extends React.Component {
-  setup(p5, whereToPlot) {
-    p5.createCanvas(500, 500, p5.WEBGL).parent(whereToPlot)
+      pipeBands$ = zipSamples(channelData$).pipe(
+        bandpassFilter({
+          cutoffFrequencies: [
+            animateSettings.cutOffLow,
+            animateSettings.cutOffHigh,
+          ],
+          nbChannels: animateSettings.nbChannels,
+        }),
+        epoch({
+          duration: animateSettings.duration,
+          interval: animateSettings.interval,
+          samplingRate: animateSettings.srate,
+        }),
+        fft({ bins: animateSettings.bins }),
+        powerByBand(),
+        catchError((err) => {
+          console.log(err);
+        })
+      );
+
+      multicastBands$ = pipeBands$.pipe(multicast(() => new Subject()));
+
+      multicastBands$.subscribe((data) => {
+        brain.current = {
+          delta: 10 * data.alpha[0],
+          theta: 10 * data.alpha[1],
+          alpha: 10 * data.alpha[2],
+          beta: 10 * data.alpha[3],
+          gamma: 10 * data.alpha[4],
+          textMsg: "Data received",
+        };
+      });
+
+      multicastBands$.connect();
+    }
   }
-  draw(p5) {
-    HEIGHT = p5.height
-    WIDTH = p5.width;
-    MOUSEX = p5.mouseX;
-    MOUSEY = p5.mouseY;
 
-    DELTA = brain.delta;
-    THETA = brain.theta;
-    ALPHA = brain.alpha;
-    BETA = brain.alpha;
-    GAMMA = brain.gamma;
-
-    //Change the code here:
-
-    p5.background(255,200,200);
-    p5.fill(0,0,0);
-    p5.stroke(10,10,10);
-    // p5.noStroke();
-    p5.ellipse(MOUSEX-250,MOUSEY-250,DELTA*10);
-    // p5.rect(40,120,120,40);
-    // p5.triangle(30,10,32,10,31,8);
-    // p5.translate();
-    // p5.rotate();
-
-
-    //Done change below here
-  }
-  render() {
-    return (
-       <Sketch setup={this.setup} draw={this.draw} />
-    )
-  }
-}
-render(
-  <MySketch />
-)
-
-
-`;
-
-        if (sketchPop) {
-          return (
-            <React.Fragment key={"dum"}>
-              <ButtonGroup>
-                <Button
-                  onClick={() => {
-                    sketchPopChange();
-                  }}
-                  primary={true}
-                  disabled={false}
-                >
-                  {"Run Code and Show Sketch"}
-                </Button>
-              </ButtonGroup>
-              <LiveProvider
-                code={code}
-                scope={scope}
-                noInline={true}
-                theme={theme}
-              >
-                <LiveEditor />
-                <LiveError />
-                <LivePreview />
-              </LiveProvider>
-            </React.Fragment>
-          );
-        } else {
-          return (
-            <React.Fragment key={"dum"}>
-              <ButtonGroup>
-                <Button
-                  onClick={() => {
-                    sketchPopChange();
-                  }}
-                  primary={true}
-                  disabled={false}
-                >
-                  {"Run Code and Show Sketch"}
-                </Button>
-              </ButtonGroup>
-              <LiveProvider
-                code={code}
-                scope={scope}
-                noInline={true}
-                theme={theme}
-              >
-                <LiveEditor />
-              </LiveProvider>
-            </React.Fragment>
-          );
-        }
-      } else {
-        // if single channel
-        return null;
+  function renderCharts() {
+    const scope = { styled, brain, React, Sketch };
+    const code = `
+    class MySketch extends React.Component {
+      setup(p5, whereToPlot) {
+        p5.createCanvas(500, 500, p5.WEBGL).parent(whereToPlot)
       }
-    });
+
+      draw(p5) {
+        HEIGHT = p5.height
+        WIDTH = p5.width;
+        MOUSEX = p5.mouseX;
+        MOUSEY = p5.mouseY;
+        DELTA = brain.current.delta;
+        THETA = brain.current.theta;
+        ALPHA = brain.current.alpha;
+        BETA = brain.current.alpha;
+        GAMMA = brain.current.gamma;
+        //Change the code here:
+        p5.background(255,200,200);
+        p5.fill(0,0,0);
+        p5.stroke(10,10,10);
+        // p5.noStroke();
+        p5.ellipse(MOUSEX-250,MOUSEY-250,DELTA);
+        // p5.rect(40,120,120,40);
+        // p5.triangle(30,10,32,10,31,8);
+        // p5.translate();
+        // p5.rotate();
+        //Done change below here
+      }
+
+      render() {
+        return (
+           <Sketch setup={this.setup} draw={this.draw} />
+        )
+      }
+
+    }
+
+    render (
+      <MySketch />
+    )
+    `;
+
+    return (
+      <LiveProvider code={code} scope={scope} noInline={true} theme={theme}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    );
   }
 
   return (
     <Card>
       <Card.Section>
-        <div style={chartStyles.wrapperStyle.style}>{RenderCharts()}</div>
+        {console.log(brain)}
+        <div style={chartStyles.wrapperStyle.style}>{renderCharts()}</div>
       </Card.Section>
     </Card>
   );
